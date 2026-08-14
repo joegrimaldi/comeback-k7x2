7f3f4eb5cbb3f5f9fcff4be31e45d9dded9569ba
joegrimaldi <jgrimaldi@gmail.com>
2026-08-14 11:15:26 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'comeback-v1';
+const SHELL = ['./', 'index.html', 'manifest.webmanifest', 'icon-180.png', 'icon-512.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  if (url.pathname.endsWith('data.enc')) {
+    // network-first: fresh numbers when online, last-known when not
+    e.respondWith(fetch(e.request).then(r => {
+      const cp = r.clone(); caches.open(CACHE).then(c => c.put('data.enc', cp)); return r;
+    }).catch(() => caches.match('data.enc')));
+  } else {
+    e.respondWith(caches.match(e.request, {ignoreSearch:true}).then(r => r || fetch(e.request)));
+  }
+});
